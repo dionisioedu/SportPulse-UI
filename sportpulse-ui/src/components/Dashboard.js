ae610a0d643cfb1165cc70a67066fb74cb9e37bc
Dionisio <ceduardodfernandes@gmail.com>
2025-04-05 21:26:02 -0300

    fix: Remove unused calls

--- sportpulse-ui/src/components/Dashboard.js
+++ sportpulse-ui/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 // src/components/Dashboard.js
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import LeagueSidebar from './LeagueSidebar';
 import SportMenu from './SportMenu';
 import CountryCards from './CountryCards';
@@ -9,7 +9,7 @@ import './Dashboard.css';
 const Dashboard = () => {
     const apiUrl = process.env.REACT_APP_API_URL;
 
-    const [error, setError] = useState('');
+    const [error] = useState('');
 
     const handleSelectSport = (sport) => {
         console.log("Sport selected:", sport);
